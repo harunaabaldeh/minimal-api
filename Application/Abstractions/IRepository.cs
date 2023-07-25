d2f8ab4bc9e06b7367dcd7bcc60d99c7c66db892
harunaabaldeh <harunaabaldeh@gmail.com>
2023-07-25 15:47:38 +0000

    initial commit

---
+++ Application/Abstractions/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Models;
+
+namespace Application.Abstractions
+{
+    public interface IRepository
+    {
+        Task<ICollection<Post>> GetPostsWithComments();
+        Task<Post> GetPostWithComment(int postId);
+        Task<Post> CreatePost(Post toCreate);
+        Task<Post> UpdatePost(string updateContent, int postId);
+        Task<Post> DeletePost(int postId);
+    }
+}
