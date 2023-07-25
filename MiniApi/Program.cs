d2d5105bb9bc0736070dcef6cc6a40b74f3bb17c
harunaabaldeh <harunaabaldeh@gmail.com>
2023-07-25 16:57:29 +0000

    added MediatR and implemented Repository pattern

--- MiniApi/Program.cs
+++ MiniApi/Program.cs
@@ -1,5 +1,15 @@
+using Application.Abstractions;
+using Application.Posts.Commands;
+using DataAccess;
+using DataAccess.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+var cs = builder.Configuration.GetConnectionString("Default");
+builder.Services.AddDbContext<SocialDbContext>(opt => opt.UseSqlite(cs));
+
+builder.Services.AddScoped<IPostRepository, PostRepositories>();
 
 var app = builder.Build();
 
